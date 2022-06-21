d41e29862fc33ed0155ee08b74f1f4f9970fdd0f
raquelbrombilla <raquelbrombilla11@gmail.com]>
2022-06-21 16:53:14 -0300

    home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,71 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from '../../components/Header';
+import Balance from '../../components/Balance';
+import Movements from '../../components/Movements';
+import Actions from '../../components/Actions';
+
+const list = [
+    {
+        id: 1,
+        label: 'Boleto conta luz',
+        value: '300,90',
+        date: '17/09/2022',
+        type: 0 //despesa
+    },
+    {
+        id: 2,
+        label: 'Pix Cliente X',
+        value: '2.500,00',
+        date: '20/09/2022',
+        type: 1 //receita/entrada
+    },
+    {
+        id: 2,
+        label: 'Salário',
+        value: '7.200,00',
+        date: '22/09/2022',
+        type: 1 //receita/entrada
+    }
+]
+
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light"/>
+      
+      <Header name="Raquel Brombilla"/>
+      <Balance saldo="9.250,00" gastos="-524,00"/>
+      
+      <Actions/>
+
+      <Text style={styles.title}>Últimas movimentações</Text>
+      
+      <FlatList 
+        style={styles.list} 
+        data={list}
+        keyExtractor={ (item) => String(item.id) }
+        showsVerticalScrollIndicator={false}
+        renderItem={ ({ item }) => <Movements data={item} /> }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+  title:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    margin: 14,
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14
+  }
+});
